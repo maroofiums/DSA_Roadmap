ff382e7ff23d06af4b0d8981674ca5e153cb23d9
maroof2424 <maroof96965@gmail.com>
2026-03-21 13:51:17 +0500

    Week1 Day1

---
+++ Week1/Week1/1. TwoSum/Optimized.py
@@ -0,0 +1,21 @@
+from typing import List
+
+def twoSum(nums: List[int], target: int) -> List[int]:
+    seen = {}
+
+    for i in range(len(nums)):
+        
+        required = target - nums[i]
+
+        if required in seen:
+            return [seen[required],i]
+        
+        seen[nums[i]] = i
+
+    return []
+
+# Example Usage
+nums = [2,7,11,15]
+target = 9
+
+print(twoSum(nums,target))
